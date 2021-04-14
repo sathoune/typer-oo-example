5eaf9c60afddcfbfe6ae7efa33e72b3e1fe376a7
michal bargiel <michal5bargiel@gmail.com>
2021-04-14 22:11:57 +0200

    ✨ Add goodbye command to hacky conversion.

--- hacky_click_to_typer/base_module.py
+++ hacky_click_to_typer/base_module.py
@@ -8,9 +8,14 @@ class BaseModule(abc.ABC):
     def __init__(self):
         self.app = typer.Typer()
         self.app.command()(self.hello)
+        self.app.command()(self.goodbye)
 
     @abc.abstractmethod
     def hello(self: typer.Context, name: str):
+        pass
+
+    @abc.abstractmethod
+    def goodbye(self: typer.Context, name: str):
         pass
 
     def __call__(self):
--- hacky_click_to_typer/runner.py
+++ hacky_click_to_typer/runner.py
@@ -7,10 +7,15 @@ class MyModule(BaseModule):
     def hello(self, name: str):
         typer.echo(f"Hello, {name}")
 
+    def goodbye(self, name: str):
+        typer.echo(f"Later! {name}")
 
 class YourModule(BaseModule):
     def hello(self, name: str):
         typer.echo(f"Greetings chap!")
+
+    def goodbye(self: typer.Context, name: str):
+        typer.echo("Excuse me Sir, do you now if there is a loo somewhere here?")
 
 
 YourModule()()
